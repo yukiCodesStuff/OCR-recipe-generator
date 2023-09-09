b21424c37e1bcbd4baaaddc5ebed03330eb3033e
yukiCodesStuff <yuki.janvier@gmail.com>
2023-09-08 20:42:41 -0500

    first commit

---
+++ src/main/java/com/ocrrecgen/OCRrecipegenerator/OcrRecipeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.ocrrecgen.OCRrecipegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrRecipeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrRecipeGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ocrrecgen/OCRrecipegenerator/OcrRecipeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocrrecgen.OCRrecipegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrRecipeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
