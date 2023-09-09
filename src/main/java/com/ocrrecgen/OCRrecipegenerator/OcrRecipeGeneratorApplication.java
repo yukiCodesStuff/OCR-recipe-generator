70bea0ff44e59f953b663711fded331d6f4acc4a
yukiCodesStuff <yuki.janvier@gmail.com>
2023-09-08 20:47:08 -0500

    hello world

--- src/main/java/com/ocrrecgen/OCRrecipegenerator/OcrRecipeGeneratorApplication.java
+++ src/main/java/com/ocrrecgen/OCRrecipegenerator/OcrRecipeGeneratorApplication.java
@@ -2,12 +2,20 @@ package com.ocrrecgen.OCRrecipegenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OcrRecipeGeneratorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OcrRecipeGeneratorApplication.class, args);
+	}
+
+	@GetMapping(value = "/")
+	public String helloWorld() {
+		return "Hello World!";
 	}
 
 }
